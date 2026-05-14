ebc3968eb4ea9cc4ea2d775b20e1c5e15b200dfe
Remigit99 <aderemibiodun1000@gmail.com>
2026-05-14 06:18:38 +0100

    feat: implement MongoDB connection and graceful shutdown in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 import {app} from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+
+await connectDB();
+
 app.listen(PORT, () => {  
     console.log(`Server is running on port ${PORT}`);  
+});
+
+process.on("SIGINT", async () => {
+  console.log("\n🛑 Shutting down server gracefully...");
+
+  process.exit(0);
 });
